3f6e62fc2b5ffcc477521981d6eed7d58d2b136a
minjuleee <minju2138@hansung.ac.kr>
2025-12-31 17:30:48 +0900

    python study 01

---
+++ variables.py
@@ -0,0 +1,17 @@
+# 변수 : 메모리 영역(RAM)에 저장되어 있는 값.
+# 저장되지 않은 값은 언젠가 소멸된다.
+
+student_name = "홍길동"
+# print(student_name)
+
+student_name = "둘리"
+print(student_name)
+
+a = 5
+b = 3
+temp = a
+a = b
+b = temp
+
+print("a = ", a)
+print("b = ", b)
