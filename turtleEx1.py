3286d12a6bddbecfa990a6268cacbbc4d3ce0287
minjuleee <minju2138@hansung.ac.kr>
2026-01-02 12:21:00 +0900

    꼬북이 정사각형 만들기

---
+++ turtleEx1.py
@@ -0,0 +1,13 @@
+import turtle   # 라이브러리를 현재 프로그램으로 로딩
+
+turtle.shape('turtle')
+
+turtle.forward(300)
+turtle.right(90)
+turtle.forward(300)
+turtle.right(90)
+turtle.forward(300)
+turtle.right(90)
+turtle.forward(300)
+
+turtle.done()
