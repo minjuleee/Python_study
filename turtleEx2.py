fb2483e82ee7c47938c9f0ab31062afcabc633a0
minjuleee <minju2138@hansung.ac.kr>
2026-01-02 15:08:07 +0900

    마우스 오른쪽버튼 클릭시(꼬북이가 그림을 그리지 않고, 좌표값으로 이동하는 기능)

--- turtleEx2.py
+++ turtleEx2.py
@@ -17,6 +17,15 @@ def screenMidClick(x, y) :
   r = random.random()
   g = random.random()
   b = random.random()
+
+def screenRightClick(x, y) :
+  # 마우스 오른쪽 버튼을 클릭하면 콘솔창에 "마우스 오른쪽 버튼 눌림!" 과 좌표값 출력하세요
+  # 꼬북이가 그림을 그리지 않으면서 x, y 좌표로 이동되도록 코딩 해보세요.
+  # hint : penup()
+  print("마우스 오른쪽 버튼 눌림! ", x, y)
+  # turtle.pencolor((r, g, b))
+  turtle.penup()
+  turtle.goto(x, y)
   
   
 # 전역변수 : 현재 파일이 로딩 되어 실행이되면 메모리에 생성, 끝나면 종료
@@ -29,5 +38,6 @@ turtle.pensize(pSize)
 
 turtle.onscreenclick(screenLeftClick, 1)
 turtle.onscreenclick(screenMidClick, 2)
+turtle.onscreenclick(screenRightClick, 3)
 
 turtle.done()
