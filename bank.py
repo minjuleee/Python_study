1fc6cc3054ea060c190d7ba030eccbec442fd802
minjuleee <minju2138@hansung.ac.kr>
2026-01-06 12:24:55 +0900

    은행 서비스

--- bank.py
+++ bank.py
@@ -4,6 +4,9 @@ money = int(input("월 납입 가능한 금액을 입력해주세요 >> "))
 
 totalMoney = 0
 month = 1
-while totalMoney >= goalMoney:
-  money * rate * 0.01 * (month(month + 1)/2 * 12)
+while totalMoney < goalMoney:
+  totalMoney = money * month + (money * rate * 0.01 * (month * (month + 1))/(2 * 12)) * 0.846
+  month += 1
+
+print("납입 개월: ", month - 1)
 
