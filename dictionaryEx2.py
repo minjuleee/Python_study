8ba7bfa6f4af8fb8100df04597ad79038a06b022
minjuleee <minju2138@hansung.ac.kr>
2026-01-07 12:29:29 +0900

    sorted를 이용한 리스트 정렬

--- dictionaryEx2.py
+++ dictionaryEx2.py
@@ -3,7 +3,7 @@ import operator
 trainDic, trainList = {}, []
 
 trainDic = {'Thomas' : '토마스', 'Edward' : '에드워드', 'Henry' : '헨리', 
-            'Gothen' : '고든', 'James' : '제임스'}
+            'Gothen' : '고든', 'James' : '제임스', 'Henrry' : '헨리'}
 
 trainList = sorted(trainDic.items(), key = operator.itemgetter(0))
 
