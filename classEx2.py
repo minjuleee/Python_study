36e9cb32f35fab534863a46609ca2a1980a9d36c
minjuleee <minju2138@hansung.ac.kr>
2026-01-09 15:21:39 +0900

    영양성분(nutrition) 속성을 추가하고, dictionary 타입으로, 여러객체를 리스트로 관리할수 있다는 것 학습

--- classEx2.py
+++ classEx2.py
@@ -8,15 +8,16 @@ class Snack:
   weight = 0.0
   price = 0
   cnt = 0   # 과자의 총 갯수(클래스 변수: 모든 객체가 공유하는 변수)
-  qty = 0   
+  nutrition = {}
   
   # 생성자의 역할 : 멤버 변수(필드)의 값을 초기화
-  def __init__(self, productName = "", weight = 0.0, price = 0):
+  def __init__(self, productName = "", weight = 0.0, price = 0, nutrition={}):
     self.productName = productName
     self.weight = weight
     self.price = price
+    self.nutrition = nutrition
     Snack.cnt += 1
   
   def displayInfo(self) :
-    print(f"상품명 : {self.productName}, 중량 : {self.weight}, 가격 : {self.price}, 과자개수 : {Snack.cnt}")
+    print(f"상품명 : {self.productName}, 중량 : {self.weight}, 가격 : {self.price}, 과자개수 : {Snack.cnt}, 영양성분 : {self.nutrition}")
 
--- snackEx1.py
+++ snackEx1.py
@@ -2,9 +2,17 @@ from classEx2 import Snack
 
 # mySnack1 = Snack(productName=input("상품명 : "), weight=input("중량 : "), price=input("가격 : "))
 # mySnack1.displayInfo()
-
-snack1 = Snack(productName="포카칩", weight= 60.0, price=1900)
+# nutrition = {"나트륨":"200mg", "탄수화물": "18g"}
+snack1 = Snack(productName="포카칩", weight= 60.0, price=1900
+               , nutrition={"나트륨" : "20g", "탄수화물" : "18g"})
 snack1.displayInfo()
 
 snack2 = Snack(productName="감자깡", weight= 40.0, price=1200)
 snack2.displayInfo()
+
+snacks = []
+snacks.append(snack1)
+snacks.append(snack2)
+
+for s in  snacks:
+  s.displayInfo()
