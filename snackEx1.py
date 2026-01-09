43db1323525624659b721dbc03c65be39c30d320
minjuleee <minju2138@hansung.ac.kr>
2026-01-09 12:41:28 +0900

    과자 객체 2개 만들고 테스트

---
+++ snackEx1.py
@@ -0,0 +1,10 @@
+from classEx2 import Snack
+
+# mySnack1 = Snack(productName=input("상품명 : "), weight=input("중량 : "), price=input("가격 : "))
+# mySnack1.displayInfo()
+
+snack1 = Snack(productName="포카칩", weight= 60.0, price=1900)
+snack1.displayInfo()
+
+snack2 = Snack(productName="감자깡", weight= 40.0, price=1200)
+snack2.displayInfo()
