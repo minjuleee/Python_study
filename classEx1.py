8da8d58279a1d1e7500f7ec8512b47d8443c67af
minjuleee <minju2138@hansung.ac.kr>
2026-01-09 11:20:32 +0900

    매개변수가 있는 생성자 + self키워드 학습

--- classEx1.py
+++ classEx1.py
@@ -3,15 +3,36 @@ class Car :
   color = "white"
   speed = 0
   
-  # 자동차 객체가 하는 기능 -> 메서드(함수)
-  def upSpeed(speed) :
-    print("자동차의 현재 속도 : ", speed)
+  # 기본생성자 : 매개변수가 없는 생성자, (self : 자기 자신의 객체를 가리키는 키워드)
+  # def __init__(self):
+  #   print("자동차 객체 1개 생성")
   
-  def downSpeed(speed) :
-    print("자동차의 현재 속도 : ", speed)
+  def __init__(self, color, speed):
+    self.color = color
+    self.speed = speed
+  
+  # 자동차 객체가 하는 기능 -> 메서드(함수)
+  def upSpeed(self, speed) :
+    self.speed += speed
+    print("자동차의 현재 속도 : ", self.speed)
+    
+  
+  def downSpeed(self, speed) :
+    self.speed -= speed
+    print("자동차의 현재 속도 : ", self.speed)
 
 
-myCar1 = Car()  # Car 클래스로부터 기본생성자(매개변수가 없는 생성자 : 생략 될 수 있음)
-print(myCar1)
+myCar1 = Car(color="흰색", speed=60)  # Car 클래스로부터 기본생성자(매개변수가 없는 생성자 : 생략 될 수 있음)
+print(myCar1.color, "속도 :", myCar1.speed)
+
+myCar2 = Car(color="검정색", speed=30)
+print(myCar2.color, "속도 :", myCar2.speed)
+
+myCar3 = Car(color="빨간색", speed=20)
+print(myCar3.color, "속도 :", myCar3.speed)
+
+myCar1.downSpeed(30)
+myCar2.upSpeed(40)
+
   
   
